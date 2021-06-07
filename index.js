64b548293523e00bc50e85cd488f139e2d714994
Kosimov-B <kosimovbakhromjon@gmail.com>
2021-06-07 11:26:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+let data = [
+    {
+        name: "Sanoat",
+        image: "images/1.jpg"
+    },
+    {
+        name: "Qishloq Xo'jaligi",
+        image: "images/2.jpg"
+    },
+    {
+        name: "Maishiy Xizmat",
+        image: "images/3.jpg"
+    },
+    {
+        name: "Hunarmand",
+        image: "images/4.jpg"
+    },
+    {
+        name: "Savdo",
+        image: "images/5.jpg"
+    },
+    {
+        name: "Xorijiy Texnologiya",
+        image: "images/6.jpg"
+    },
+    {
+        name: "Xorijiy Hom-ashyo",
+        image: "images/7.jpg"
+    },
+    {
+        name: "Xorijiy Tayyor Mahsulot",
+        image: "images/8.jpg"
+    },
+     {
+        name: "Xorijiy Xizmat",
+        image: "images/9.jpg"
+    },
+    {
+        name: "Xorijiy Qishloq Ho'jaligi",
+        image: "images/10.jpg"
+    },
+    {
+        name: "Taklif",
+        image: "images/11.jpg"
+    },
+    {
+        name: "E'lon",
+        image: "images/12.jpg"
+    },
+    
+];
+
+
+$(document).ready(function () {
+    buildProductView(data)
+});
+// All search
+$(".all").on("click", function () {
+    buildProductView(data)
+    
+});
+
+$(".foo").on("click", function () {
+    let filterBrand = this.innerHTML;
+    let filterData = data.filter((set) => (filterBrand === set.name) ? true : false);
+    buildProductView(filterData)
+    console.log(buildProductView)
+    
+});
+// qidiruv  ----------
+$("#search").on("keyup", function(){
+    let currentText = $(this).val().toLowerCase();
+    let filtered = data.filter((obj) => {
+        let newObj = JSON.stringify(obj).toLowerCase();
+        if(newObj.includes(currentText)){
+            return true;
+        }else{
+            return false
+        }
+    });
+    buildProductView(filtered)
+});
+
+
+function buildProductView(arr) {
+    $("#cards").empty();
+    arr.forEach((set) => {
+
+        let perProduct =
+            '<div class="card" style="width: 13rem; margin:30px; border:none;">' +
+            ' <img src=" ' + set.image + ' ">' +
+            '<div class="card-body">' +
+            '  <h5 class="card-title">' + set.name + '</h5>' +
+            '</div>' +
+            ' </div>';
+        $("#cards").append(perProduct);
+    });
+
+}
+
+
+
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
